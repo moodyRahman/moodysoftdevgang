f29a2cb9559a8f53699cff103d3ca10d758329e4
Moody Rahman <mrahman01@stuy.edu>
2019-09-23 12:39:56 -0400

    xtra commit

---
+++ 10_occ_xtra/app.py
@@ -0,0 +1,60 @@
+# Moududur "Moody" Rahman
+# SoftDev1 pd2
+# K10 -- Jinja Tuning
+# 2019-09-21
+
+
+
+
+from flask import Flask, render_template
+import csvtest
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")     # this is the "homepage"
+def hello_world():
+    print(__name__) # Printed onto console on refresh
+    return '''<a href="/occupyflaskst">click here for 10_occ !!</a> <br>
+                <a href="/jeff">old index page</a> <br>
+                <a href="/my_foist_template">jinja test</a> <br>
+            '''
+
+@app.route("/jeff")   #testing a dumb method to read html
+def jeff():
+    print(__name__)
+    return open("./static/index.html").read()
+
+
+@app.route("/a")
+def a():
+    print(__name__)    # dont go here
+    return csvtest.allrows   #this is just me playing around lmao
+
+
+@app.route("/my_foist_template")
+def disp():
+    print(__name__) # Printed onto console on refresh
+    coll = [0, 1,2, 3,4, 5]
+    return render_template( "tempp.html", username = "doof")   #jinja attempt one!
+
+
+@app.route("/occupyflaskst")
+def hw1():
+    allrows = []
+
+    with open("static/occupations.csv") as csvfile:  #reads file for the html table
+        readCSV = csv.reader(csvfile, delimiter=",")
+        for row in readCSV:
+            allrows.append(row)
+
+    print(__name__) # Printed onto console on refresh
+    #twat is a 2d array of the jobs
+    #.getjob() is an imported function to return the weighted randomly chosen job
+    return render_template( "occupation.html", jobinp = csvtest.getjob(), twat = allrows[1:])
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
---
+++ 10_occ_xtra/csvtest.py
@@ -0,0 +1,56 @@
+# Moududur Rahman and Justin Chen
+# SoftDev1 pd2
+#K06 -- StI/O: Divine your Destiny!
+#2019-09-16
+
+#
+# i copy pasted the code into one big function and ipmorted it into app.py
+import csv
+
+import random
+
+def getjob():
+    allrows = []
+    alljobs = {}
+
+    with open("static/occupations.csv") as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=",")
+        for row in readCSV:
+            allrows.append(row)
+
+# print (allrows[1])
+
+    alljobs[allrows[1][0]] = float(allrows[1][1])
+
+# print (alljobs[allrows[1][0]])
+
+    for x in range(1, len(allrows)):
+        alljobs[allrows[x][0]] = float(allrows[x][1]) * 10
+
+
+    alljobs.pop("Total")
+
+    def getjob():
+        low = 0
+        high = 0
+        selector = random.randint(0, 997)
+        for job in alljobs.keys():
+                high = low + alljobs[job]
+                if low <= selector and selector < high:
+                    return job
+                low = high
+
+    # testdict = {}
+    #
+    # for x in alljobs.keys():
+    #     testdict[x] = 0
+    #
+    # for x in range(1000):
+    #     z = getjob()
+    #     #print(z)
+    #     testdict[z] += 1
+
+    return getjob()
+
+
+getjob()

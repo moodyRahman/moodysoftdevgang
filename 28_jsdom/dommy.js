9842b1ef32b3dc953b6b252726a51878db1ecf16
Moody Rahman <mrahman01@stuy.edu>
2019-12-12 14:30:49 -0500

    fib list

--- 28_jsdom/dommy.js
+++ 28_jsdom/dommy.js
@@ -1,12 +1,15 @@
 
-var list = ["<li>item 0</li>",
-"<li>item 1</li>",
-"<li>item 2</li>",
-"<li>item 3</li>",
-"<li>item 4</li>",
-"<li>item 5</li>",
-"<li>item 6</li>",
-"<li>item 7</li>]"];
+var list = ["<li id=0>item 0</li>",
+"<li id=1>item 1</li>",
+"<li id=2>item 2</li>",
+"<li id=3>item 3</li>",
+"<li id=4>item 4</li>",
+"<li id=5>item 5</li>",
+"<li id=6>item 6</li>",
+"<li id=7>item 7</li>]"];
+
+
+var fiblist = []
 
 var addItem = function(e){
   listdom = document.getElementById("thelist");
@@ -34,10 +37,39 @@ var removeItem = function(e){
   listdom.innerHTML = out;
 };
 
+var fibonacci = function (n) {
+    if (n == 0) {
+        return 0;
+    }
+    if (n == 1) {
+        return 1;
+    }
+    return (fibonacci(n - 1) + fibonacci(n - 2));
+};
+
+
+
+currfib = 0;
+
+
+var addFib = function(e){
+  fibdom = document.getElementById("fiblist");
+  fiblist.push("<li>" + fibonacci(currfib) + "</li>");
+  currfib = currfib + 1;
+  out = fiblist.toString();
+  while (out.indexOf(',') != -1) {
+    out = out.replace(/,/, "");
+  }
+  out = out.replace("]", "")
+  out = out.replace("[", "")
+  console.log(out)
+  fibdom.innerHTML = out;
+};
 
 
 document.getElementById("b").addEventListener("click", function(){addItem();});
 document.getElementById("thelist").addEventListener("click", function(){removeItem();} );
+document.getElementById("fb").addEventListener("click", function(){addFib()});
 
 function triggerEvent( elem, event ) {
   var clickEvent = new Event( event ); // Create the event.
